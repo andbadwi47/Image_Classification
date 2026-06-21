4a03489d893c2d8514458102e8e5139803a2e6cb
andbadwi47 <andbadwi47@gmail.com>
2026-06-21 22:18:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,44 @@
 import streamlit as st
 import tensorflow as tf
+import tempfile
 import numpy as np
 from PIL import Image
 
-@st.cache_resource
-def load_model():
-    return tf.keras.models.load_model("ct_scan_classifier_model.h5")
+st.title("CT Scan Classification")
 
-model = load_model()
-
-IMG_SIZE = (224, 224)
-
-CLASS_NAMES = [
-    "COVID",
-    "NORMAL",
-    "PNEUMONIA"
-]
-
-def predict(image):
-    image = image.convert("RGB")
-    image = image.resize(IMG_SIZE)
-
-    img = np.array(image).astype(np.float32) / 255.0
-    img = np.expand_dims(img, axis=0)
-
-    prediction = model.predict(img, verbose=0)
-
-    predicted_index = np.argmax(prediction)
-    predicted_class = CLASS_NAMES[predicted_index]
-    confidence = float(np.max(prediction))
-
-    return predicted_class, confidence, prediction[0]
-
-st.set_page_config(page_title="CT Scan Classifier", page_icon="🩻")
-
-st.title("🩻 CT Scan Classification")
-
-uploaded_file = st.file_uploader(
-    "Upload CT Scan Image",
-    type=["jpg", "jpeg", "png"]
+# Upload model
+uploaded_model = st.file_uploader(
+    "Upload Model (.h5)",
+    type=["h5"]
 )
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+if uploaded_model is not None:
 
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".h5") as tmp:
+        tmp.write(uploaded_model.read())
+        model_path = tmp.name
 
-    with st.spinner("Menganalisis gambar..."):
-        predicted_class, confidence, probs = predict(image)
+    model = tf.keras.models.load_model(model_path)
 
-    st.success(f"Hasil Prediksi: {predicted_class}")
-    st.metric("Confidence", f"{confidence*100:.2f}%")
+    st.success("Model berhasil dimuat!")
 
-    prob_dict = {
-        CLASS_NAMES[i]: float(probs[i])
-        for i in range(len(CLASS_NAMES))
-    }
+    uploaded_image = st.file_uploader(
+        "Upload CT Scan",
+        type=["jpg", "jpeg", "png"]
+    )
 
-    st.bar_chart(prob_dict)
+    if uploaded_image is not None:
+
+        image = Image.open(uploaded_image).convert("RGB")
+
+        st.image(image)
+
+        img = image.resize((224, 224))
+
+        img = np.array(img) / 255.0
+        img = np.expand_dims(img, axis=0)
+
+        prediction = model.predict(img)
+
+        st.write("Prediksi:")
+        st.write(prediction)
